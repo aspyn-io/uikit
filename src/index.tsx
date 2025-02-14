61fbd6b09178494a2edf08ec4fc0b9c6bd6af7c6
Chase Pierce <syntaqx@gmail.com>
2025-02-14 00:49:51 -0700

    export NoteCard globally

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export { Modal } from "./components/Modal";
 export { Sidebar } from "./components/Sidebar";
 export { SidebarProvider } from "./context/SidebarContext";
 export { Navbar } from "./components/Navbar/Navbar";
+export { NoteCard } from "./components/NoteCard";

be3560a73cd7aa76356c84e587db083303c8986e
Neal Thacker <neal.thacker@goaptive.com>
2025-02-17 16:06:07 -0500

    Add AppointmentCard export to index

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,4 @@ export { Sidebar } from "./components/Sidebar";
 export { SidebarProvider } from "./context/SidebarContext";
 export { Navbar } from "./components/Navbar/Navbar";
 export { NoteCard } from "./components/NoteCard";
+export { AppointmentCard } from "./components/AppointmentCard";

f904692a33238a133057079ed17a1d81ef1ca560
Chase Pierce <syntaqx@gmail.com>
2025-02-27 17:28:34 -0700

    no onboarding

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,15 +1,24 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+
+  docs: {
+    autodocs: true,
+  },
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
   },
 };
 export default config;

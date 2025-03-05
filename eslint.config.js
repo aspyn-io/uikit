5349ab4e8ff228b2ca0a653c7b428f1fb90c31f6
Andrey Trunov <andrey.trunov@goaptive.com>
2025-03-05 22:06:30 +0200

    Update linter config to ensure empty line at the end of .ts and .tsx files

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', '**/*.stories.tsx'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -33,6 +33,7 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'eol-last': ['error', 'always']
     },
   },
 ]

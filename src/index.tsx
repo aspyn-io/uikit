e7f3f7443f41535cfe1c120705a1a40d8d147053
Andrey Trunov <andrey.trunov@goaptive.com>
2025-03-05 21:32:47 +0200

    Add export for searchable select interfaces

--- src/index.tsx
+++ src/index.tsx
@@ -18,4 +18,4 @@ export { InvoiceDetail } from "./components/InvoiceDetail";
 export { NavbarProvider, useNavbarContext } from "./context/NavbarContext";
 
 // Form Inputs
-export { SearchableSelect } from "./components/SearchableSelect";
+export { SearchableSelect, SearchableSelectProps, SearchableOption } from "./components/SearchableSelect";

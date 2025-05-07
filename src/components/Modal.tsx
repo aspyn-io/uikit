229c979d50ecf53b96d9e4b0d02300f9691f7798
Chase Pierce <syntaqx@gmail.com>
2025-05-06 20:05:42 -0600

    dismissable modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -14,39 +14,9 @@ export const Modal: React.FC<ModalProps> & {
 } = ({ show, onClose, children, ...rest }) => {
   const modalRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const handleEsc = (event: KeyboardEvent) => {
-      if (event.key === "Escape" && onClose) {
-        onClose();
-      }
-    };
-
-    const handleClickOutside = (event: MouseEvent) => {
-      if (
-        modalRef.current &&
-        !modalRef.current.contains(event.target as Node) &&
-        onClose
-      ) {
-        onClose();
-      }
-    };
-
-    if (show) {
-      document.addEventListener("keydown", handleEsc);
-      document.addEventListener("mousedown", handleClickOutside);
-    } else {
-      document.removeEventListener("keydown", handleEsc);
-      document.removeEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("keydown", handleEsc);
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [show, onClose]);
-
   return (
     <FlowbiteModal
+      dismissible
       theme={{
         root: {
           show: {
